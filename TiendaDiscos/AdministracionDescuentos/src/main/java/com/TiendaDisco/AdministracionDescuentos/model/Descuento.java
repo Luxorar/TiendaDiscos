943a9027fafdddb2dcc8c50cb30638b31fec0183
Albedo-AB <di.aravenab@duocuc.cl>
2026-04-30 16:22:34 -0400

    DTO's Descuentos Diego

--- TiendaDiscos/AdministracionDescuentos/src/main/java/com/TiendaDisco/AdministracionDescuentos/model/Descuento.java
+++ TiendaDiscos/AdministracionDescuentos/src/main/java/com/TiendaDisco/AdministracionDescuentos/model/Descuento.java
@@ -14,8 +14,8 @@ import java.util.ArrayList;
 public class Descuento {
     private Long id;
     @NotBlank(message = "Ingrese nombre del descuento") String nombre;
-    private ArrayList<Producto> productosConDescuento = new ArrayList<>();
-    private ArrayList<Disco> discosConDescuento = new ArrayList<>();
+    private ArrayList<Producto> productosConDescuento = new ArrayList();
+    private ArrayList<Disco> discosConDescuento = new ArrayList();
     private Estado estado;
     private double descuento;
 }
