db2c53a123c936d6019c6e6f1dea6a6532f9b536
Fernandemon2906 Castillo <fern.castilloa@duocuc.cl>
2026-05-03 18:39:28 -0400

    Creacion de metodos en repository

--- TiendaDiscos/RegistroResenas/src/main/java/com/TiendaDisco/RegistroResenas/repository/ResenaRepository.java
+++ TiendaDiscos/RegistroResenas/src/main/java/com/TiendaDisco/RegistroResenas/repository/ResenaRepository.java
@@ -1,7 +1,5 @@
 package com.TiendaDisco.RegistroResenas.repository;
-import com.TiendaDisco.RegistroResenas.model.Disco;
 import com.TiendaDisco.RegistroResenas.model.Resena;
-import com.TiendaDisco.RegistroResenas.model.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -10,9 +8,41 @@ import java.util.ArrayList;
 public interface ResenaRepository {
     ArrayList<Resena> listaResenas = new ArrayList<>();
 
-    ArrayList<Disco> listaDiscos = new ArrayList<>();
+    Long contador = 1L;
 
-    ArrayList<User> listaUsers = new ArrayList<>();
+    default Resena postResena(Resena r){
+        listaResenas.add(r);
+        return r;
+    }
 
+    default Resena getResenaId(Long id){
+        for(Resena r: listaResenas) {
+            if (id == r.getId()) {
+                return r;
+            }
+        }
+        return null;
+    }
+
+    default String putResena(Long id, Resena r){
+        for(Resena res : listaResenas){
+            if(res.getId()==id){
+                res.setMensaje(r.getMensaje());
+
+                return "Actualizacion exitosa";
+            }
+        }
+        return "Los cmabios no se han realizado";
+    }
+
+    default String deleteResena(Long id){
+        for(Resena r:listaResenas){
+            if(r.getId() == id){
+                listaResenas.remove(r);
+                return "Resena eliminada";
+            }
+        }
+        return "Resena no encontrada";
+    }
 
 }
