e59070198f081912cef6398b45825a1b46cf9dd5
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-04 09:54:38 -0400

    diego

--- TiendaDiscos/AdministracionDescuentos/src/main/java/com/TiendaDisco/AdministracionDescuentos/Repository/DescuentoRepository.java
+++ TiendaDiscos/AdministracionDescuentos/src/main/java/com/TiendaDisco/AdministracionDescuentos/Repository/DescuentoRepository.java
@@ -6,5 +6,38 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public interface DescuentoRepository {
+    ArrayList<Descuento> ListaDescuentos = new ArrayList<>();
+
+    default List<Descuento> ObtenerTodos() {
+        return ListaDescuentos;
+    }
+
+    default Descuento ObtenerPorId(long id) {
+        for (Descuento d : ListaDescuentos) {
+            if (d.getId().equals(id)) {
+                return d;
+            }
+        }
+        return null;
+    }
+
+    default Descuento ObtenerPorNombre(String nombre) {
+        for (Descuento d : ListaDescuentos) {
+            if (d.getNombre().equalsIgnoreCase(nombre)) {
+            }
+        }
+        return null;
+    }
+
+    default boolean eliminar(Long id) {
+        for (int i = 0; i < ListaDescuentos.size(); i++) {
+            if (ListaDescuentos.get(i).getId().equals(id)) {
+                ListaDescuentos.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
 }
