0a0a07da1aaf636c296febad527bd7d6e4011cc9
Fernando Castillo <fern.castilloa@duocuc.cl>
2026-05-17 15:23:18 -0400

    Cambio request mapping

--- TiendaDiscos/RegistrarSede/src/main/java/com/TiendaDisco/RegistrarSede/controller/SedeController.java
+++ TiendaDiscos/RegistrarSede/src/main/java/com/TiendaDisco/RegistrarSede/controller/SedeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/envios")
+@RequestMapping("api/v1/Sede")
 public class SedeController {
     @Autowired
     private SedeService service;
