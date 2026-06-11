89ede8947550d46ad64e0a94d78069d817943e7b
Albedo-AB <di.aravenab@duocuc.cl>
2026-06-11 17:17:21 -0400

    swagger (arreglar)

---
+++ RegistroResenas/src/main/java/com/TiendaDisco/RegistroResenas/config/OpenApiConfig.java
@@ -0,0 +1,46 @@
+package com.TiendaDisco.RegistroResenas.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI configurarOpenApi(){
+        // Información de contacto
+        Contact contacto = new Contact()
+                .name("TeamSpica")
+                .email("lu.villalonc@duocuc.cl")
+                .url("https://www.duoc.cl");
+
+        // Licencia del proyecto
+        License licencia = new License()
+                .name("MIT")
+                .url("https://opensource.org/licences/MIT");
+
+        // Información principal de la API
+        Info informacionApi = new Info()
+                .description("""
+                        Microservicio para
+                        administrar resenas
+                        """)
+                .version("1.0")
+                .termsOfService("https://www.duoc.cl")
+                .contact(contacto)
+                .license(licencia);
+        // Documentación externa (GitHub)
+        ExternalDocumentation github = new ExternalDocumentation()
+                .description("Repositorio oficial del proyecto en GitHub")
+                .url("https://github.com/Luxorar/TiendaDiscos");
+        // Configuración OpenAPI
+        return new OpenAPI()
+                .info(informacionApi)
+                .externalDocs(github);
+    }
+}
--- RegistroResenas/src/main/java/com/TiendaDisco/RegistroResenas/controller/ResenaController.java
+++ RegistroResenas/src/main/java/com/TiendaDisco/RegistroResenas/controller/ResenaController.java
@@ -2,26 +2,64 @@ package com.TiendaDisco.RegistroResenas.controller;
 
 import com.TiendaDisco.RegistroResenas.model.Resena;
 import com.TiendaDisco.RegistroResenas.service.ResenaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/Resena")
+@Tag(
+        name="Resenas",
+        description = "Se registran las resenas de los discos"
+)
 public class ResenaController {
     @Autowired
     private ResenaService service;
 
+    @Operation(
+            summary="Registro de una resena",
+            description="Permite agregar una nueva resena activa de un disco"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode="201",
+                    description="Resena creada"),
+            @ApiResponse(responseCode = "400",
+                    description = "Datos invalidos")
+    })
     @PostMapping
     public Resena postResena(@RequestBody Resena resena){
         return service.postResena(resena);
     }
 
+    @Operation(
+            summary="Obtencion de resena con id",
+            description="Obtiene una resena en base a su id"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode="200",
+                    description = "Obtencion exitosa"),
+            @ApiResponse(responseCode="400",
+                    description = "Datos invalidos")
+    })
     @GetMapping("{id}")
     public Resena getResenaId(@Valid @PathVariable Long id){
         return service.getResenaId(id);
     }
 
+    @Operation(
+            summary="Eliminar resena",
+            description="Eliminar resena en base a su id"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode="200",
+                    description="Eliminacion exitosa"),
+            @ApiResponse(responseCode="400",
+                    description="Datos invalidos")
+    })
     @DeleteMapping("{id}")
     public String deleteResenaId(@PathVariable Long id){
         return service.deleteResena(id);
