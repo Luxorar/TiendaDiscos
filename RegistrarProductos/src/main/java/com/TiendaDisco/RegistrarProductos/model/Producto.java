a70eacbf364805047d3613b5ca2b7f4f20a67098
Fernando Castillo <fern.castilloa@duocuc.cl>
2026-06-15 19:48:54 -0400

    swagger registrar productos

--- RegistrarProductos/src/main/java/com/TiendaDisco/RegistrarProductos/model/Producto.java
+++ RegistrarProductos/src/main/java/com/TiendaDisco/RegistrarProductos/model/Producto.java
@@ -1,5 +1,6 @@
 package com.TiendaDisco.RegistrarProductos.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -7,20 +8,44 @@ import lombok.*;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor @Builder @Entity
 @Table(name = "PRODUCTO")
+@Schema(
+        name = "Producto",
+        description = "microservicio dedicado a administrar productos"
+)
 public class Producto {
 
+    @Schema(
+            title = "id",
+            description = "Identificador unico de los productos",
+            example = "1"
+    )
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Schema(
+            title = "Nombre",
+            description = "Nombre del producto",
+            example = "Tornamesa"
+    )
     @NotBlank(message = "Se debe ingresar un nombre de producto")
     @Column(name = "nombre_producto")
     private String nombreProducto;
 
+    @Schema(
+            title = "Marca",
+            description = "Marca del producto",
+            example = "audio-technica"
+    )
     @NotBlank(message = "Se debe ingresar una marca")
     @Column(name = "marca")
     private String marca;
 
+    @Schema(
+            title = "precio",
+            description = "Precio del producto",
+            example = "250000"
+    )
     @NotNull(message = "Se debe ingresar un precio")
     @Column(name = "precio")
     private Integer precio;
