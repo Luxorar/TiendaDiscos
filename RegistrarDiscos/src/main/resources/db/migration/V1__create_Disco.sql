02453d44f9b9d4f3c43c6a68d69fa5f42426273a
DiegoBarria <dieg.barriam@duocuc.cl>
2026-06-24 16:24:09 -0400

    Fix: cambia AUTO_INCREMENT a BIGSERIAL para PostgreSQL

--- RegistrarDiscos/src/main/resources/db/migration/V1__create_Disco.sql
+++ RegistrarDiscos/src/main/resources/db/migration/V1__create_Disco.sql
@@ -1,14 +1,13 @@
 CREATE TABLE TITULO(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(50) NOT NULL
+                       id BIGSERIAL PRIMARY KEY,
+                       titulo VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE DISCOS(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    nombre  VARCHAR(50) NOT NULL,
-    artista VARCHAR(50) NOT NULL,
-    precio  BIGINT NOT NULL,
-    titulo_id BIGINT NOT NULL,
-    FOREIGN KEY (titulo_id) REFERENCES TITULO(ID)
+                       id BIGSERIAL PRIMARY KEY,
+                       nombre VARCHAR(50) NOT NULL,
+                       artista VARCHAR(50) NOT NULL,
+                       precio BIGINT NOT NULL,
+                       titulo_id BIGINT NOT NULL,
+                       FOREIGN KEY (titulo_id) REFERENCES TITULO(ID)
 );
-
