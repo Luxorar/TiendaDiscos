5274c7eece499bc70a411fb863ec05a1eaf9ffc8
DiegoBarria <dieg.barriam@duocuc.cl>
2026-07-15 01:43:23 -0400

    feat: Agregados 5 discos nuevos vía Flyway y forzada subida de imagenes a uploads

---
+++ RegistrarDiscos/src/main/resources/db/migration/V6__insert_mas_discos.sql
@@ -0,0 +1,14 @@
+INSERT INTO discos (nombre, artista, precio, imagen)
+VALUES ('The Dark Side of the Moon', 'Pink Floyd', 25000, 'pink_floyd_dark_side.jpg');
+
+INSERT INTO discos (nombre, artista, precio, imagen)
+VALUES ('Thriller', 'Michael Jackson', 22000, 'michael_jackson_thriller.jpg');
+
+INSERT INTO discos (nombre, artista, precio, imagen)
+VALUES ('Corazones', 'Los Prisioneros', 15000, 'los_prisioneros_corazones.jpg');
+
+INSERT INTO discos (nombre, artista, precio, imagen)
+VALUES ('Formula, Vol. 2', 'Romeo Santos', 18000, 'romeo_santos_formula_v2.jpg');
+
+INSERT INTO discos (nombre, artista, precio, imagen)
+VALUES ('All Things Must Pass', 'George Harrison', 28000, 'george_harrison_all_things.jpg');
